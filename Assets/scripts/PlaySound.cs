94c6e14ab8dd444808c56833c9d459fcf97fd30a
BloodBusk <thomashbusk@gmail.com>
2022-05-03 12:07:45 +0200

    ...

--- Assets/scripts/PlaySound.cs
+++ Assets/scripts/PlaySound.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class PlaySound : MonoBehaviour
 {
     public AudioSource playSound;
+    double counter = 0;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
-            playSound.gameObject.SetActive(true);
-        }else if (playSound.gameObject.activeInHierarchy) {
-                 playSound.gameObject.SetActive(false);
-             }
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            counter = counter + 1;
+            Debug.Log("shots fired");
+            playSound.Play();
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("shots stopped: " + counter);
+            playSound.Stop();
+        }
     }
-
 }
