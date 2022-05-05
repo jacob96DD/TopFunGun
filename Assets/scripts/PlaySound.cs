4d5f8fc6cb1df64fa859287a7b4ea0aadc7c5698
jacob96DD <jacob110396@outlook.com>
2022-05-05 10:19:37 +0200

    Revert "..."
    This reverts commit 94c6e14ab8dd444808c56833c9d459fcf97fd30a.

--- Assets/scripts/PlaySound.cs
+++ Assets/scripts/PlaySound.cs
@@ -5,21 +5,16 @@ using UnityEngine;
 public class PlaySound : MonoBehaviour
 {
     public AudioSource playSound;
-    double counter = 0;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            counter = counter + 1;
-            Debug.Log("shots fired");
-            playSound.Play();
-        }
-        else if (Input.GetKeyUp(KeyCode.Space))
-        {
-            Debug.Log("shots stopped: " + counter);
-            playSound.Stop();
-        }
+        if(Input.GetMouseButtonDown(0)){
+            playSound.gameObject.SetActive(true);
+        }else if (playSound.gameObject.activeInHierarchy) {
+                 playSound.gameObject.SetActive(false);
+             }
+        
     }
+
 }
